d5744d87d0b5f896c53675106a781b8f035724c1
onyyyyy <hongki365@gmail.com>
2023-11-15 14:15:51 +0900

    231114 복습

---
+++ src/pages/home/Banner.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { IMG_URL } from "../../constants";
+
+const MainBanner = styled.div`
+  height: 80vh;
+  background-color: lightgray;
+  background: url(${IMG_URL}/original/${(props) => props.$bgUrl}) no-repeat
+    center / cover;
+  padding: 400px 5%;
+  h3,
+  p {
+    position: relative;
+  }
+
+  h3 {
+    max-width: 600px;
+    width: 100%;
+    font-size: 80px;
+    font-weight: 700;
+    margin-bottom: 30px;
+    letter-spacing: -2px;
+    line-height: 100px;
+  }
+
+  p {
+    max-width: 600px;
+    width: 100%;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 26px;
+    opacity: 0.8;
+  }
+
+  @media screen and (max-width: 450px) {
+    h3 {
+      font-size: 50px;
+      line-height: 65px;
+    }
+
+    p {
+      font-size: 16px;
+    }
+  }
+`;
+const BlackBg = styled.div`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    8deg,
+    rgba(0, 0, 0, 1) 22%,
+    rgba(255, 255, 255, 0.5080882694874824) 100%
+  );
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const Banner = ({ BannerData }) => {
+  return (
+    <MainBanner $bgUrl={BannerData.backdrop_path}>
+      <BlackBg />
+      <h3>{BannerData.title}</h3>
+      <p>{BannerData.overview.slice(0, 100) + "..."}</p>
+    </MainBanner>
+  );
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,65 +1,33 @@
-import { useEffect } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from "react";
 import { nowPlaying } from "../../api";
-
-const MainBanner = styled.div`
-  height: 80vh;
-  background-color: lightgray;
-  padding: 400px 5%;
-  h3,
-  p {
-    position: relative;
-  }
-
-  h3 {
-    font-size: 80px;
-    font-weight: 700;
-    margin-bottom: 30px;
-    letter-spacing: -2px;
-    line-height: 100px;
-  }
-
-  p {
-    font-size: 18px;
-    font-weight: 400;
-    line-height: 26px;
-    opacity: 0.8;
-  }
-`;
-const BlackBg = styled.div`
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    8deg,
-    rgba(0, 0, 0, 1) 22%,
-    rgba(255, 255, 255, 0.5080882694874824) 100%
-  );
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
+import { Banner } from "./Banner";
 
 export const Home = () => {
+  const [nowPlayingData, setNowPlayingData] = useState();
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
     (async () => {
       try {
-        const data = await nowPlaying();
-        console.log(data);
+        const { results } = await nowPlaying();
+        setNowPlayingData(results);
+        setLoading(false);
+        // console.log(data);
       } catch (error) {
         console.log("에러:" + error);
       }
     })();
   }, []);
+
+  console.log(isLoading);
+  console.log(nowPlayingData);
   return (
-    <MainBanner>
-      <BlackBg />
-      <h3>영화소개</h3>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam quas
-        perferendis dolorem asperiores molestias ex vero aliquam eaque, numquam
-        saepe temporibus, laudantium cumque fuga alias nulla. Eaque cumque
-        placeat reprehenderit.
-      </p>
-    </MainBanner>
+    <>
+      {isLoading ? (
+        "loading..."
+      ) : (
+        <>{nowPlayingData && <Banner BannerData={nowPlayingData[0]} />}</>
+      )}
+    </>
   );
 };
